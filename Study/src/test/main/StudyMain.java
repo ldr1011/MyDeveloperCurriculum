f3db0bbf949567f0496ecbed9ff699c66b301e99
LeeDongRyul <lol.trust10@gmail.com>
2020-11-23 21:40:21 +0900

    Study 프로젝트 추가됨

---
+++ Study/src/test/main/StudyMain.java
@@ -0,0 +1,7 @@
+package test.main;
+
+public class StudyMain {
+	public static void main(String[] args) {
+		System.out.println("스터디 시작합니다.");
+	}
+}
---
+++ Study/src/test/main/StudyMain02.java
@@ -0,0 +1,9 @@
+package test.main;
+
+public class StudyMain02 {
+	public static void main(String[] args) {
+		System.out.println("스터디중");
+		System.out.println("스터디");
+		System.out.println("공부");
+	}
+}
